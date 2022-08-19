da81eda1a3009d38fce8bf52b53803208c5f429c
Ngô Quốc Trung <trungngotdt@outlook.com.vn>
2022-08-19 16:12:03 +0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string parameterFolderImages = "--folder";
+            const string parameterHeight = "--height";
+            const string parameterWidth = "--width";
+            const string parameterFileName = "--file";
+            const string parameterPagesIndex = "--page-index";
+            const string parameterType = "--type";
+
+            Console.WriteLine("Working");
+            int pages = -1;
+            string folderImage = String.Empty;
+            int height = -1;
+            int width = -1;
+            string fileName = String.Empty;
+            string typeImage = string.Empty;
+
+            var length = args.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                switch (args[i])
+                {
+                    case parameterPagesIndex:
+                        int.TryParse(args[i + 1], out pages);
+                        break;
+                    case parameterHeight:
+                        int.TryParse(args[i + 1], out height);
+                        break;
+                    case parameterWidth:
+                        int.TryParse(args[i + 1], out width);
+                        break;
+                    case parameterType:
+                        typeImage = args[i + 1];
+                        break;
+                    case parameterFileName:
+                        fileName = args[i + 1];
+                        break;
+                    case parameterFolderImages:
+                        folderImage = args[i + 1];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            if (pages == -1 || height == -1 || width == -1 || string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(folderImage) || string.IsNullOrEmpty(typeImage))
+            {
+                Console.WriteLine("Empty parameters!");
+                return;
+            }
+
+            ConvertIamges2PDF.Converter.Instance.CompressImagesPDF(width, height, fileName,pages, folderImage, typeImage);
+        }
+    }
+}
---
+++ ConvertIamges2PDF/Converter.cs
@@ -0,0 +1,80 @@
+﻿using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConvertIamges2PDF
+{
+    public class Converter
+    {
+
+        private static Converter instance = null;
+        private static readonly object padlock = new object();
+        private Converter()
+        {
+
+        }
+        public static Converter Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Converter();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void CompressImagesPDF(int width, int height, string fileName, int pages, string imagesFolder, string typeImage)
+        {
+            try
+            {
+                var isFilesExist = Directory.GetFiles(imagesFolder, "*." + typeImage).Length > 0;
+                if (!isFilesExist)
+                {
+                    Console.WriteLine("No files in " + imagesFolder);
+                    return;
+                }
+
+                using PdfDocument pdfDocument = new PdfDocument(new PdfWriter(Path.Combine(Directory.GetCurrentDirectory(), fileName)));
+                Document document = new Document(pdfDocument);
+                for (int i = 0; i < pages; i++)
+                {
+                    string file = Path.Combine(imagesFolder, i.ToString()+"." + typeImage);
+                    Console.WriteLine("Compressing " + file);
+                    ImageData imageData = ImageDataFactory.Create(file);
+                    Image image = new Image(imageData);
+                    image.SetWidth(width);
+                    image.SetHeight(height);
+                    document.Add(image);
+                }
+                pdfDocument.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                LoggerMethod(ex);
+                throw;
+            }
+
+        }
+
+        private void LoggerMethod(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+        }
+    }
+}
